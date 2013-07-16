3e51e805b1061c46789ab10a3369ee19c33b8789
Edouard <edou4rd@gmail.com>
2013-07-16 18:43:30 +0200

    dataset for europe

--- plugin.php
+++ plugin.php
@@ -95,4 +95,26 @@ class DatawrapperPlugin_VisualizationMaps extends DatawrapperPlugin_Visualizatio
             "options" => $this->getOptions()
         );
     }
+
+    public function getDemoDataSets(){
+        $id = $this->getName();
+        $datasets = array();
+        $datasets[] = array(
+            'id' => 'unemployment-rate-in-the-european-union',
+            'title' => __('Unemployment rate in the European Union', $id),
+            'type' => __('Europe Map', $id),
+            'presets' => array(
+                'type' => 'maps',
+                'metadata.describe.intro' => __("The unemployment rate is the percentage of unemployed in the labor force, on the basis of the definition of the International Labour Organization (ILO).", $id),
+                'title' => __('Unemployment rate in the European Union', $id),
+                'metadata.describe.source-name' => 'Eurostat',
+                'metadata.describe.source-url' => 'http://epp.eurostat.ec.europa.eu/tgm/table.do?tab=table&init=1&plugin=1&language=fr&pcode=teilm020',
+                'metadata.data.vertical-header' => true,
+                'metadata.data.transpose' => false,
+                'metadata.visualize.map' => 'europe'
+            ),
+            'data' => "code ISO;Taux de chomage Janvier 2012\nDE;5,4\nAT;4,8\nBE;8,1\nBG;12,6\nCY;13,6\nDK;7,4\nES;26,4\nEE;9,9\nFI;8\nFR;10,8\nGR;26,7\nHU;11,2\nIE;14,1\nIT;11,7\nLV;\nLT;13,1\nLU;5,4\nMT;6,7\nNL;6\nPL;10,6\nPT;17,5\nCZ;7,1\nRO;6,6\nGB;7,8\nSK;14,5\nSI;9,6\nSE;8"
+        );
+    return $datasets;
+    }
 }
