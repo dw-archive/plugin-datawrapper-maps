7a347095f2839d2c27c7948769a0ed30afd2030c
Gregor Aisch <contact@vis4.net>
2013-09-29 02:35:06 +0200

    refined map options

--- plugin.php
+++ plugin.php
@@ -80,28 +80,22 @@ class DatawrapperPlugin_VisualizationMaps extends DatawrapperPlugin_Visualizatio
     private function getOptions() {
         $id = $this->getName();
         return array(
-            "---map-options---" => array(
-                "type" => "separator",
-                "label" => __("Select and customize display", $id)
-            ),
+
             "map" => array(
                 "type"    => "map-selector",
                 "label"   => __("Select map", $id),
                 "options" => $this->getMapsAsOption(),
             ),
-            "---color-options---" => array(
-                "type" => "separator",
-                "label" => __("Customize map colors", $id)
-            ),
             // NOTE: doesn't working, breaks the chart if used. Nothing else can be changed after.
-            "color-column" => array(
+            "map-columns" => array(
                 "type" => "select-axis-column",
-                "axis" => "color",
-                "default" => 0,
-                "label" => __("Select data column", $id)
-                /*"depends-on" => array(
-                    "chart.min_columns[color]" => 2
-                )*/
+                "axes" => array(array(
+                    "id" => "keys",
+                    "label" => __("Key column")
+                ), array(
+                    "id" => "color",
+                    "label" => __("Data column")
+                ))
             ),
             "gradient" => array(
                 "type" => "color-gradient-selector",
@@ -119,6 +113,15 @@ class DatawrapperPlugin_VisualizationMaps extends DatawrapperPlugin_Visualizatio
             //     ),
             //     "keys" => "color"
             // )
+            "---other-options---" => array(
+                "type" => "separator",
+                "label" => __("Other settings", $id)
+            ),
+            "fit-into-chart" => array(
+                "type" => "checkbox",
+                "label" => __("Fit map into chart size"),
+                "default" => false
+            )
         );
     }
 
